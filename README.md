8034e9460532789fa38e4fb8f86be56efbfb4328
Dict Xiong <xiong.dian@qq.com>
2019-03-15 23:17:36 +0800

    190315

---
+++ README.md
@@ -0,0 +1,36 @@
+# OneDirUp
+
+onedrivecmd Directory Upload
+
+## 功能
+
+使用递归的方式弥补了 onedrivecmd 不带目录上传的功能, 且能够在失败时自动重试. 
+
+此处的失败仅限于: python 抛出异常, 或者提示 `Annotations must be specified before other elements in a JSON object`. 重试次数目前为无限次. 
+
+## 安装
+
+需要先安装 onedrivecmd 和 python3, 然后把 onedirup.py 克隆到本地即可.
+
+## 使用
+
+运行
+
+`python3 onedirup.py "/tmp/hello" "od:/foo/bar/" [--dry-run] [--no-try-again]`
+
+其中: 
+
+`--dry-run`: 测试上传, 列出要上传的文件以及命令, 但是不执行.
+
+`--no-try-again`: 禁用在失败时自动重试功能.  
+
+原地址和目标地址的引号**最好加上**, 路径中有空格时必须加上. 
+
+**源**可以是文件夹, 也可以是文件, 结尾的斜杠 `/` 可加可不加. 
+
+**目标地址**一定是一个 OneDrive 中的文件夹, 结尾的斜杠也可随意.
+
+**运行结果**: 无论是文件还是文件夹, `hello` 都会被上传到 `od:/foo/bar/` 下, 也就是 `od:/foo/bar/hello`. 
+
+
+
--- onedirup.py
+++ onedirup.py
@@ -1,6 +1,8 @@
-import os,sys
+import os,sys,re
 from time import sleep
 
+global conf
+conf={}
 # upload files recursively. src[-1] shouldn't be "/" but dest[-1] must be "/".
 def upload(src,dest):
 	if (os.path.isdir(src)):
@@ -10,11 +12,35 @@ def upload(src,dest):
 			upload(src+"/"+file,ndest)
 	else:
 		cmd="onedrivecmd put \""+src+"\" \""+dest+"\""
-		print("\033[32m >"+cmd+"\033[0m")
-		os.system(cmd)
+		print("\n\033[36m>"+cmd+"\033[0m")
+		if "--dry-run" in conf:
+			print("Not exec because of '--dry-run'.")
+		else:
+			if "--no-try-again" not in conf:
+				while (True):
+					print("Try transfering...")
+					try:
+						tmp=os.popen(cmd).read()
+					except:
+						print("\033[31mTransfer failed, will try again later.\033[0m")
+						sleep(2)
+						continue
+					print("\033[33mshell message:\033[0m "+tmp)
+					if (re.search("Annotations",tmp)==None):
+						print("\033[4mTransfer finished!\033[0m")
+						break
+					else:
+						print("\033[31mTransfer failed, will try again later.\033[0m")
+						sleep(2)
+					
+			else:
+				os.system(cmd)
+				print(1)
 
 src=sys.argv[1]
 dest=sys.argv[2]
+for i in range(3,len(sys.argv)):
+	conf[sys.argv[i]]=True
 if (src[-1]=='/'):
 	src=src[:-1]
 if (dest[-1]!="/"):
